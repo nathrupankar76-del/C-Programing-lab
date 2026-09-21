cc994e2305164a02d62c4f27c41cb0b7d45124d7
nathrupankar76-del <nathrupankar76@gmail.com>
2026-09-21 11:21:15 +0530

    Add files via upload

---
+++ rupanakar.cpp
@@ -0,0 +1,84 @@
+//write a c proggram which read an integer value and cneck wheather it is possesive or not, after satisfying the integer value>0.if it is possetive then check it is oddor even
+//# include<stdio.h>
+//int main()
+//{
+//	int num1,num2,num3;
+//	printf("enter first number");
+//	scanf("%d",&num1);
+//	printf("enter second number");
+//	scanf("%d",&num2);
+//	printf("enter third number");
+//	scanf("%d",&num3);
+//	if(num1>num2&& num1>num3)
+//	{
+//		printf("the first number is greatest...%d");
+//	}
+//	else if(num2>num3)
+//	{
+//		printf("the second number is greatest....%d");
+//	}
+//	else{
+//		printf("the thired number is greatest...%d");
+//	}
+//}
+//write a c program to calculate the electrcity bill based on the number of units consumed
+//the charges  are:0-100=2rs per unit,201-300=5rs per unit,301+7 rs/unit 
+//#include<stdio.h>
+//int main()
+//{
+//	int unit;
+//	printf("enter number of unit consumed:");
+//	scanf("%d",&unit);
+//	if(unit<=100)
+//	{
+//		printf("electricity bill=%d",2000000*2);
+//	}
+//	else if(unit<=200)
+//	{
+//		printf("electricity bil=%d",(100*2)+(unit-100)*3);
+//	}
+//	else if(unit<=300)
+//	{
+//		printf("electricity bill=%d",(100*2)+(100*3)+(unit-200)*5);
+//	}
+//	else{
+//		printf("electricity bill=%d",(100*2)+(100*3)+(100*5)+(unit-300)*7);
+//	}
+//}
+//write a c proggram to print 1-5 using in while loop
+//#include<stdio.h>
+//int main()
+//{
+//	int i=1;
+//	while(i<=5)
+//     {
+//     	printf("%d\n",i);
+//     	i++;
+//	}	
+//	return 0;
+//	
+//}
+//write a c program to to print 5-1 using on while loop 
+//#include<stdio.h>
+//int main ()
+//{
+//  int i=5;
+//   while(i>=1)
+//   {
+//   printf("%d\n",i);
+//   i--;
+//   }  
+//   return 0;	
+//}
+//w. a. p. to print even numbers to 10
+//#include<stdio.h>
+//int main()
+//{
+//	int i=1;
+//	while(i<=10)
+//	{
+//		if(i%2!=0)
+//		printf("odd number is:%d\n",i);
+//		i++;
+//	}
+//}
